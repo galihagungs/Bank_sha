ba039de19b6b07f19a805901455c02913859a250
Galih Agung Sukmawan <galihagung223@gmail.com>
2023-07-23 23:21:48 +0700

    Routing Page

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:bank__sha/shared/theme.dart';
-import 'package:bank__sha/ui/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -18,11 +17,7 @@ class _SplashPageState extends State<SplashPage> {
     super.initState();
 
     Timer(const Duration(seconds: 3), () {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const OnboardingPage(),
-          ));
+      Navigator.pushNamed(context, '/onboarding');
     });
   }
 

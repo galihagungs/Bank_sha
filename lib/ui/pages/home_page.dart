b0fdefcf994dd55f4f1acbdd52835bcb3217b983
Galih Agung Sukmawan <galihagung223@gmail.com>
2023-07-25 15:01:17 +0700

    Overview Page Part1

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,6 +8,203 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        elevation: 0,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          backgroundColor: whiteColor,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+              ),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_statistic.png',
+                width: 20,
+              ),
+              label: 'Statistic',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Reward',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          buildProfile(),
+          buildWalletCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy,',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'shaynahan',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semibold,
+                ),
+              ),
+            ],
+          ),
+          // const Spacer(),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/img_profile.png'),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWalletCard() {
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/img_bg_card.png'),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Shayna Hanna',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+          const SizedBox(
+            height: 28,
+          ),
+          Row(
+            children: [
+              Text(
+                '**** **** **** 1280',
+                style: whiteTextStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: medium,
+                  letterSpacing: 7.5,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 21,
+          ),
+          Text(
+            'Balance',
+            style: whiteTextStyle.copyWith(
+              fontSize: 14,
+            ),
+          ),
+          Text(
+            'Rp 12.500',
+            style: whiteTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: semibold,
+            ),
+          )
+        ],
+      ),
     );
   }
 }

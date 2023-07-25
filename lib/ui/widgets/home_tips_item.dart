1f777d54eb6dd75978da3170305ed2dc2b339851
Galih Agung Sukmawan <galihagung223@gmail.com>
2023-07-25 20:59:07 +0700

    url  Laucher Fix

--- lib/ui/widgets/home_tips_item.dart
+++ lib/ui/widgets/home_tips_item.dart
@@ -1,5 +1,6 @@
 import 'package:bank__sha/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeTipsItem extends StatelessWidget {
   final String imgUrl;
@@ -15,42 +16,51 @@ class HomeTipsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 155,
-      height: 176,
-      decoration: BoxDecoration(
-        color: whiteColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-            child: Image.asset(
-              width: 155,
-              height: 110,
-              imgUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Text(
-              title,
-              style: blackTextStyle.copyWith(
-                fontWeight: medium,
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () async {
+        Uri newUrl = Uri.parse(url);
+        if (await canLaunchUrl(newUrl)) launchUrl(newUrl);
+        // if (!await launchUrl(newUrl)) {
+        //   throw Exception('Could not launch $newUrl');
+        // }
+      },
+      child: Container(
+        width: 155,
+        height: 176,
+        decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
               ),
-              maxLines: 2,
+              child: Image.asset(
+                width: 155,
+                height: 110,
+                imgUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 12,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Text(
+                title,
+                style: blackTextStyle.copyWith(
+                  fontWeight: medium,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                maxLines: 2,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
